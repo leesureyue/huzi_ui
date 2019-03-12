29afd3cc6da0f7add4f42dae6b9863063a87dd7e
lee <lee@gmail.com>
2019-03-12 15:24:57 +0800

    complete the [roject

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,14 +4,6 @@ import {Layout,Menu,Icon} from "antd";
 const {Header,Footer,Sider,Content} = Layout;
 const SubMenu = Menu.SubMenu;
 
-class Analysis extends Component{
 
-    render(){
-        return (
-            <div>Hello </div>
-        )
-    }
-    
-}
 
 export default Analysis;
