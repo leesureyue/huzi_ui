f9ade07b0389d92e0af7310950fc26cf5ef44ce4
lee <lee@gmail.com>
2019-03-12 15:24:16 +0800

    use antd

---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+import {Component} from 'react';
+import {Layout,Menu,Icon} from "antd";
+
+const {Header,Footer,Sider,Content} = Layout;
+const SubMenu = Menu.SubMenu;
+
+class Analysis extends Component{
+
+    render(){
+        return (
+            <div>Hello </div>
+        )
+    }
+    
+}
+
+export default Analysis;
